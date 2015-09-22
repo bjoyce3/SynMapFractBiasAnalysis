94cb4187236b7c4630d93afeb32815fe962cc9da
Blake Joyce <bjoyce3@email.arizona.edu>
2015-09-22 10:58:57 -0700

    Added allgenes argument per James Schnable's request. Tool can now process all genes in target genome, or only ones that are conserved in at least one chromosome of the query genome.

--- Code_FractBias/GecoFractBias/fractionation_bias_geco_from_ipython_v2.py
+++ Code_FractBias/GecoFractBias/fractionation_bias_geco_from_ipython_v2.py
@@ -15,6 +15,7 @@ parser.add_argument("--target", help="Target genome CoGe ID number. The target g
 parser.add_argument("--windowsize", help="Sets the size of the sliding window for analysis")
 #parser.add_argument("--queryID", help="Query genome CoGe ID number. The query genome is the one with more subgenomes. Used to determine which side of the SynMap output file the target genome data exists.")
 parser.add_argument("--output", help="File path to output directory. Specify SynMap directory: 'storage/coge/data/diags' file path.")
+parser.add_argument("--allgenes", help="Determines whether all genes in target genome gff or only conserved genes should be used in analysis")
 args = parser.parse_args()
 
 
@@ -37,15 +38,14 @@ from collections import OrderedDict
 import matplotlib.pyplot as plt
 import numpy as np 
 import matplotlib.gridspec as gridspec
-
+import seaborn as sns
 
 #had to install this using pip on local computer
-from natsort import natsorted, natsort_key
-import matplotlib
+from natsort import natsorted
 
 #PATH TO RUN IN DESKTOP
 #Sorghum/Maize comparison
-#python fractionation_bias_geco.py --align /home/bjoyce3/sorghum_maize/Sorghum_maize_synmap_results.txt --gff /home/bjoyce3/sorghum_maize/Sorghum_bicolor_annos1-cds0-id_typename-nu1-upa1-add_chr0.gid6807.gff --target 6807 --windowsize 100 --output /home/bjoyce3/sorghum_maize/
+#python fractionation_bias_geco_from_ipython_v2.py --align /home/bjoyce3/sorghum_maize/SorghumMaize_gcoords_merge60_syntenicdepth40.txt --gff /home/bjoyce3/sorghum_maize/Sorghum_bicolor_6807_prodrun.gff --target 6807 --windowsize 100 --output /home/bjoyce3/sorghum_maize/ --allgenes True
 
 #pina/rice comparison
 #python fractionation_bias_geco.py --align /home/bjoyce3/pinarice/SynMapKsMerge120Sdepth10_Osativa-Acomosus2-1Acomv6.txt --gff /home/bjoyce3/pinarice/Ananas_comosus_pineapple_v6.gff --target 25734 --windowsize 100 --output /home/bjoyce3/pinarice
@@ -132,6 +132,7 @@ t0 = datetime.now()
 with open(synmap_import_file, 'r') as f:  # open SynMap file containing syntenic genes
     synmap_rowcount = 0
     cols = []  # list for parsing columns from SynMap data
+    decimal_strip_check_target_gene = 0
     for line in f:  # for loop to parse columns
         new_line = line.replace('||', '\t')  #converts || into tabs for universal delimination
         if line[0] != '#' and line[0] != '\n' and line[0] != '{' and line[0] != '}' and (line[0] != "\\"):  #sorts out columns containing syntenic block information/headings
@@ -145,10 +146,13 @@ with open(synmap_import_file, 'r') as f:  # open SynMap file containing syntenic
                 #clean ID subgenome A from the column on left of data
                 ida = cols[0]
                 ida = ida[1:cols[0].index('_')]
-            if args_target == ida:
+            if args.target == ida:
                 if 'scaffold' not in cols[1] and 'Scaffold' not in cols[1]:
                     target_chr = cols[1]
                     target_gene = str(cols[4]).rstrip('.')  #puts all genome1_genes with synteny into a list
+                    decimal_strip_check_target_gene = target_gene.find('.')
+                    if not decimal_strip_check_target_gene == (-1):
+                        target_gene = target_gene[:(decimal_strip_check_target_gene)]
                 if 'scaffold' not in cols[13] and 'Scaffold' not in cols[13]:
                     query_chr = str(cols[13])  #puts all genome2_chrs with synteny to genes in genome1 into a list
                     query_gene = str(cols[16]).rstrip('.')  #puts all genome2_genes with synteny to genes in a genome1 into a list
@@ -156,6 +160,9 @@ with open(synmap_import_file, 'r') as f:  # open SynMap file containing syntenic
                 if 'scaffold' not in cols[13] and 'Scaffold' not in cols[13]:
                     target_chr = cols[13]
                     target_gene = str(cols[16])  #puts all genome1_genes with synteny into a list
+                    decimal_strip_check_target_gene = target_gene.find('.')
+                    if not decimal_strip_check_target_gene == (-1):
+                        target_gene = target_gene[:(decimal_strip_check_target_gene)]
                 if 'scaffold' not in cols[1] and 'Scaffold' not in cols[1]:
                     query_chr = str(cols[1])  #puts all genome2_chrs with synteny to genes in genome1 into a list
                     query_gene = str(cols[4])  #puts all genome2_genes with synteny to genes in a genome1 into a list
@@ -227,35 +234,82 @@ with open(str(fract_bias_raw_output_file), 'w') as csvfile:
     writer = csv.writer(csvfile, dialect='excel', delimiter=',', lineterminator='\n')
     writer.writerow(headers)
 
-    for tchr in gff_genes:
-        col0 = chr #writes Pineapple chr number
-        count = 0
-        for diction in gff_genes[tchr]:
-            gene = diction['gene_name']
-            col1 = gene #writes pineapple gene name
-            count += 1
-            col2 = count #writes pineapple gene number on pineapple chr
-            #Find the query chr genes and output to columns: first gff info (col0-3), query chr (col 4-n), query chr-gene (col n+1-m)
-            syntenic_query_gene_presence_data = []
-            syntenic_query_gene_name = []
-            for qchr in query_lst:
-                if not tchr in windanalysis_input_dict:
-                    windanalysis_input_dict[tchr] = {}  #initializes the nested dictionary-primary level at genome1_chromosome
-                if not qchr in windanalysis_input_dict[tchr]:
-                    windanalysis_input_dict[tchr][qchr] = []  #initializes first nesting in dictionary-second level at genome1_genes
-                try:
-                    syn_gene = d[tchr][gene][qchr]
-                    syntenic_query_gene_presence_data.append(True)
-                    syntenic_query_gene_name.append(syn_gene)
-                    windanalysis_input_dict[tchr][qchr].append(True)
-                except KeyError:
-                    syntenic_query_gene_presence_data.append(False)
-                    syntenic_query_gene_name.append(".")
-                    windanalysis_input_dict[tchr][qchr].append(False)
-            rows = [tchr, col1, col2]
-            rows.extend(syntenic_query_gene_presence_data)
-            rows.extend(syntenic_query_gene_name)
-            writer.writerow(rows)
+    if args.allgenes == True:
+        for tchr in gff_genes:
+            col0 = chr #writes Pineapple chr number
+            count = 0
+            for diction in gff_genes[tchr]:
+                gene = diction['gene_name']
+                col1 = gene #writes pineapple gene name
+                count += 1
+                col2 = count #writes pineapple gene number on pineapple chr
+                #Find the query chr genes and output to columns: first gff info (col0-3), query chr (col 4-n), query chr-gene (col n+1-m)
+                syntenic_query_gene_presence_data = []
+                syntenic_query_gene_name = []
+                for qchr in query_lst:
+                    if not tchr in windanalysis_input_dict:
+                        windanalysis_input_dict[tchr] = {}  #initializes the nested dictionary-primary level at genome1_chromosome
+                    if not qchr in windanalysis_input_dict[tchr]:
+                        windanalysis_input_dict[tchr][qchr] = []  #initializes first nesting in dictionary-second level at genome1_genes
+                    try:
+                        syn_gene = d[tchr][gene][qchr]
+                        syntenic_query_gene_presence_data.append(True)
+                        syntenic_query_gene_name.append(syn_gene)
+                        windanalysis_input_dict[tchr][qchr].append(True)
+                    except KeyError:
+                        syntenic_query_gene_presence_data.append(False)
+                        syntenic_query_gene_name.append("")
+                        windanalysis_input_dict[tchr][qchr].append(False)
+                rows = [tchr, col1, col2]
+                rows.extend(syntenic_query_gene_presence_data)
+                rows.extend(syntenic_query_gene_name)
+                writer.writerow(rows)
+
+    elif args.allgenes == False:
+        for tchr in gff_genes:
+            col0 = chr #writes Pineapple chr number
+            count = 0
+            for diction in gff_genes[tchr]:
+                gene = diction['gene_name']
+                col1 = gene #writes pineapple gene name
+                count += 1
+                col2 = count #writes pineapple gene number on pineapple chr
+                #Find the query chr genes and output to columns: first gff info (col0-3), query chr (col 4-n), query chr-gene (col n+1-m)
+                syntenic_query_gene_presence_data = []
+                syntenic_query_gene_name = []
+                for qchr in query_lst:
+                    if not tchr in windanalysis_input_dict:
+                        windanalysis_input_dict[tchr] = {}  #initializes the nested dictionary-primary level at genome1_chromosome
+                    if not qchr in windanalysis_input_dict[tchr]:
+                        windanalysis_input_dict[tchr][qchr] = []  #initializes first nesting in dictionary-second level at genome1_genes
+                    try:
+                        syn_gene = d[tchr][gene][qchr]
+                        syntenic_query_gene_presence_data.append(True)
+                    except KeyError:
+                        syntenic_query_gene_presence_data.append(False)
+
+                if sum(syntenic_query_gene_presence_data) >= 1:
+                    for qchr in query_lst:
+                        try:
+                            syn_gene = d[tchr][gene][qchr]
+                            syntenic_query_gene_name.append(syn_gene)
+                            windanalysis_input_dict[tchr][qchr].append(True)
+                        except KeyError:
+                            syntenic_query_gene_name.append("")
+                            windanalysis_input_dict[tchr][qchr].append(False)
+                    rows = [tchr, col1, col2]
+                    rows.extend(syntenic_query_gene_presence_data)
+                    rows.extend(syntenic_query_gene_name)
+                    writer.writerow(rows)
+
+                elif sum(syntenic_query_gene_presence_data) < 1:
+                    continue
+                else:
+                    print 'Target gene not classified'
+                    continue
+
+    else:
+        print "Genes to be used (all genes or at least one syntenic gene) are not defined"
 
 t4 = datetime.now()
 
@@ -306,8 +360,9 @@ try:
     output_dict = collections.OrderedDict(sorted(output_dict.items(), key=lambda t: alphanumbsort(*re.match(r'([a-zA-Z]+)(\d+)',t[0]).groups())))
 except AttributeError:
     #sorted(output_dict, key=lambda x: output_dict[x])
-    natsorted(output_dict[tchr])
-    
+    #natsorted(output_dict[tchr])
+    print "Alphanumeric sort didn't work"
+
 t5 = datetime.now()
 
 
@@ -335,53 +390,63 @@ with open(retention_calc_output_file, 'wb') as csvf:
 print "Plotting FractBias Data"
 
 #define figure size, column layout, grid layout
-figsize = (15, 20)
+figsize = (15, (len(target_lst)*2.4))
 cols = 2
 gs = gridspec.GridSpec(len(output_dict) // cols + 1, cols)
 
-# These are the "Tableau 20" colors as RGB  
-tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),  
-             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),  
-             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),  
-             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),  
-             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)] 
-  
-# Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts  
-for i in range(len(tableau20)):  
-    r, g, b = tableau20[i]  
+
+
+# These are the "Tableau 20" colors as RGB
+tableau20 = [(31, 119, 180), (174, 199, 232), (255, 127, 14), (255, 187, 120),
+             (44, 160, 44), (152, 223, 138), (214, 39, 40), (255, 152, 150),
+             (148, 103, 189), (197, 176, 213), (140, 86, 75), (196, 156, 148),
+             (227, 119, 194), (247, 182, 210), (127, 127, 127), (199, 199, 199),
+             (188, 189, 34), (219, 219, 141), (23, 190, 207), (158, 218, 229)]
+
+# Scale the RGB values to the [0, 1] range, which is the format matplotlib accepts
+for i in range(len(tableau20)):
+    r, g, b = tableau20[i]
     tableau20[i] = (r / 255., g / 255., b / 255.)
 
-fig = plt.figure(figsize=figsize)
+#Alternate color palettes Seaborn/Brewer
+
+current_palette = sns.color_palette("Set2", 40)
+#tableau20
+
+fig = plt.figure(figsize=figsize, frameon=False)
 subplt_count = -1
 ax = []
-
 for tchr in output_dict:
     subplt_count += 1
-    print "Plotting target chromosome: "+str(subplt_count)
-    count = 0 
+    print "Plotting subplot: "+str(subplt_count)
+    count = 0
     row = (subplt_count // cols)
     col = subplt_count % cols
     ax.append(fig.add_subplot(gs[row, col]))
-    
     for qchr in output_dict[tchr]:
         count += 1
-        #print output_dict[tchr][qchr]
         if (max(output_dict[tchr][qchr].itervalues()))>0:
             x = output_dict[tchr][qchr].keys()
             y = output_dict[tchr][qchr].values()
+            #Sets up plotting conditions
             ax[-1].spines["top"].set_visible(False)
             ax[-1].spines["right"].set_visible(False)
+            ax[-1].spines["left"].set_visible(True)
+            ax[-1].spines["bottom"].set_visible(True)
+            ax[-1].patch.set_visible(False)
             ax[-1].get_xaxis().tick_bottom()
             ax[-1].get_yaxis().tick_left()
-            ax[-1].plot(x, y, color=tableau20[count], lw=2)
-            ax[-1].set_title(label='Target Chromosome: '+species_name_filter+" "+ tchr, fontweight='bold', fontsize=14)
-            ax[-1].set_xlabel('Window Iteration\n(Gene order number on chromosome)', fontsize=12, fontweight='bold')
-            ax[-1].set_ylabel('Retention %\n(# Syntenic Genes/window size)', fontsize=12, fontweight='bold')
-            ax[-1].legend(loc=1, frameon=False, title="Query Chromosome", fontsize=10)
+            ax[-1].plot(x, y, color=current_palette[count], lw=2, label=str(qchr))
+            ax[-1].set_title(label='Target Chromosome: '+species_name_filter+" "+ tchr, fontweight='bold', fontsize=14, y=1.1, loc='left')
+            ax[-1].set_xlabel('Window Iteration\n(Gene number)', fontsize=12, fontweight='bold')
+            ax[-1].set_ylabel('% Retention\n(#Syntenic genes/window size)', fontsize=12, fontweight='bold')
+            ax[-1].legend(bbox_to_anchor=(1.25, 1.1), loc=1, frameon=False, title="       Query\nChromosome", fontsize=10)
+
         else:
             continue
-fig.tight_layout()
-plt.savefig(args.output+"html/"+"fractbias_figure1.png") #<--------
+
+fig.subplots_adjust(wspace=0.45, hspace=0.6)
+plt.savefig(args.output+"/html/"+"fractbias_figure1.png", transparent=True) #<--------
 
 t6 = datetime.now()
 
